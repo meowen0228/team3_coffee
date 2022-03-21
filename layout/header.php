a81fc146b0291d6528ebd97edfa69c03dea726ca
CHIA0524 <101316993+CHIA0524@users.noreply.github.com>
2022-03-21 13:52:20 +0800

    通知數字

--- layout/header.php
+++ layout/header.php
@@ -16,7 +16,9 @@ $total_u = $pdo->query($u_sql)->fetch(PDO::FETCH_NUM)[0];
    
           
     <div class="col-1 ">
-      <i class="fa-solid fa-bell d-flex flex-column" ></i>
+      <i class="fa-solid fa-bell d-flex flex-column" >
+      <div id="bell123"><?= $total_o+$total_u ?></div>
+      </i>
       <div id="bell" >
         <a href="order_list.php" class="a_php">
         <div class="notify" onclick="od_no()">
