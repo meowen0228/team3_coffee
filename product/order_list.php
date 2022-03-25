7a9deb264d1f05928e610237c632c2fa43e6f30c
CHIA0524 <love860524boy@gmail.com>
2022-03-25 14:38:07 +0800

    資料庫更新

--- product/order_list.php
+++ product/order_list.php
@@ -325,8 +325,8 @@ if ($totalRows) {
 
 
 </main>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
     //給collapse
 </script>
 <script>
@@ -374,6 +374,18 @@ if ($totalRows) {
             $(this).next().attr("href", "order_list_search.php?search-for=" + search);
         }
     });
+
+    $(".collapsed").click(function() {
+    var itemHref = $(this).attr("href");
+    var itemClass = $(itemHref).attr("class");
+    if (itemClass === "panel-collapse collapse") {
+        $(itemHref).attr("class", "panel-collapse collapse in").css("height", "auto");
+    } else {
+        $(itemHref).attr("class", "panel-collapse collapse").css("height", "0px");
+    }
+    return false; //停止運行bootstrap自帶的函數
+    });
+    
 </script>
 
 <?php include '../layout/scripts.php'; ?>
